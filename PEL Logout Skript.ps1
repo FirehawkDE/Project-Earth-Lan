32535d4a91f70dd96f73aa4dce22b8c5ee63ef5e
Alex Meiss <firehawk215@googlemail.com>
2026-07-31 09:11:29 +0200

    Delete PEL Logout Skript.ps1

--- PEL Logout Skript.ps1
+++
@@ -1,4 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-$networkId = "091f0945fc5012f1"
-zerotier-cli leave $networkId
-[System.Windows.Forms.MessageBox]::Show("Verbindung zu Netzwerk $networkId getrennt.", "ZeroTier", "OK", "Information")
