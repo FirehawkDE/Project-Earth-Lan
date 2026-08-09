7f7dd4572cd987c5c6e1746cd3a6860a3e4641b1
Alex Meiss <firehawk215@googlemail.com>
2026-08-09 18:57:39 +0200

    Update PES Logout.ps1

--- PES Logout.ps1
+++ PES Logout.ps1
@@ -1,4 +1,4 @@
 Add-Type -AssemblyName System.Windows.Forms
-$networkId = "091f0945fc5012f1"
+$networkId = "Deine Netzwerk-ID"
 zerotier-cli leave $networkId
 [System.Windows.Forms.MessageBox]::Show("Verbindung zu Netzwerk $networkId getrennt.", "ZeroTier", "OK", "Information")
